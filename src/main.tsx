68a6a29bec6448471d0a206c79df43e880320ed8
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-08-07 15:25:58 -0300

    feat: add query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import { AppRoutes } from './routes'
 
 import './styles/index.css'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRoutes />
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
