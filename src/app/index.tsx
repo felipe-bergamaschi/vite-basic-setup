4d8dd0328aeecbd6234774fd528fc44f1ff7557a
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-08-07 15:43:31 -0300

    feat: add request

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Fragment } from 'react'
+import { useGetAxiosAxios } from '../query'
+
 import '../styles/App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const { data, isLoading } = useGetAxiosAxios()
+
+  if (isLoading) {
+    return <h1>loading...</h1>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+    <Fragment>
+      <h1> Repo: {data?.data.name}</h1>
+
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {data?.data.description}
       </p>
-    </>
+
+      <strong>👀 {data?.data.subscribers_count}</strong>{' '}
+      <strong>✨ {data?.data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data?.data.forks_count}</strong>
+    </Fragment>
   )
 }
