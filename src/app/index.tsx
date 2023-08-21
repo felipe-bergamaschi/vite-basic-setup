7ec4e52cb61a0ccf8149b345a755e814ae5cd6d5
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-08-21 12:43:47 -0300

    feat: add error & refetch

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,10 +4,14 @@ import { useGetAxiosAxios, useGetTanStackQuery } from '../query'
 import '../styles/App.css'
 
 export default function App() {
-  const { data, isLoading } = useGetAxiosAxios()
+  const { data, isLoading, isError, refetch } = useGetAxiosAxios()
 
   if (isLoading || !data) {
     return <h1>loading...</h1>
+  }
+
+  if (isError) {
+    return <h1>error...</h1>
   }
 
   return (
@@ -21,6 +25,8 @@ export default function App() {
       <strong>👀 {data.subscribers_count}</strong>{' '}
       <strong>✨ {data.stargazers_count}</strong>{' '}
       <strong>🍴 {data.forks_count}</strong>
+
+      <button onClick={() => refetch()}>Refetch data</button>
     </Fragment>
   )
 }
